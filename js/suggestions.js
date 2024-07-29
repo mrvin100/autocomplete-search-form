a7efd66cd73aa89f3c674b482c1a323b428f9528
vincent youmssi <vincentyoumssi@gmail.com>
2024-07-29 12:45:13 +0100

    first commit

---
+++ js/suggestions.js
@@ -0,0 +1,33 @@
+// Search sugestions
+
+let suggestions = [ 
+"Channel",
+"#jeandoe",
+"jeandoe developper",
+"jeandoe youtube programing",
+"jeandoe youtube templates",
+"jeandoe javascripts projects",
+"YouTube",
+"YouTuber",
+"YouTube Channel",
+"Blogger",
+"Nollywood",
+"Vlogger",
+"Vechiles",
+"Facebook",
+"Freelancer",
+"Facebook Page",
+"Designer",
+"Developer",
+"Web Designer",
+"Web Developer",
+"Login Form in HTML & CSS",
+"How to learn HTML & CSS",
+"How to learn JavaScript",
+"How to became Freelancer",
+"How to became Web Designer",
+"How to start Gaming Channel",
+"How to start YouTube Channel",
+"What does HTML stands for?",
+"What does CSS stands for?"
+];
